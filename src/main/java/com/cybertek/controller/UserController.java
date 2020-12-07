2bf16b721ec469e2c82be7a820ce1a968da826f1
abaiaz487 <baiazbekova@gmail.com>
2020-12-07 10:19:47 -0500

    project

--- src/main/java/com/cybertek/controller/UserController.java
+++ src/main/java/com/cybertek/controller/UserController.java
@@ -3,14 +3,25 @@ package com.cybertek.controller;
 
 import com.cybertek.dto.RoleDTO;
 import com.cybertek.dto.UserDTO;
+import com.cybertek.entity.Role;
+import com.cybertek.service.RoleService;
+import com.cybertek.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
+
+    @Autowired
+    RoleService roleService;
+
+    @Autowired
+    UserService userService;
 
     @GetMapping({"/create", "/add", "/initialize"}) //it's possible to put several endpoints
     public String createUser(Model model){
@@ -18,7 +29,22 @@ public class UserController {
         model.addAttribute("user", new UserDTO());
         //datagenerator
 
-        //model.addAttribute("roles", findAll());
+        model.addAttribute("roles", roleService.findAll());
+        model.addAttribute("users", userService.findAll());
+
+        return "/user/create";
+    }
+
+    @PostMapping("/create")
+    public String insertUser(UserDTO user, Model model){
+
+        userService.save(user);
+
+        //user, roles, users
+        model.addAttribute("user", new UserDTO());
+        model.addAttribute("roles", roleService.findAll());
+        model.addAttribute("users", userService.findAll()); //will return whatever is inside the map
+
         return "/user/create";
     }
 
