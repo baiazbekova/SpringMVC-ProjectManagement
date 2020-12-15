8eaa430b46bfa28a571d25cc82357236f2b8b85a
abaiaz487 <baiazbekova@gmail.com>
2020-12-15 11:35:42 -0500

    project

--- src/main/java/com/cybertek/controller/TaskController.java
+++ src/main/java/com/cybertek/controller/TaskController.java
@@ -2,6 +2,10 @@ package com.cybertek.controller;
 
 
 import com.cybertek.dto.TaskDTO;
+import com.cybertek.service.ProjectService;
+import com.cybertek.service.TaskService;
+import com.cybertek.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,10 +15,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/task")
 public class TaskController {
 
+    @Autowired
+    ProjectService projectService;
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    TaskService taskService;
+
+
     @GetMapping("/create")
     public String createTask(Model model){
 
+
         model.addAttribute("task", new TaskDTO());
+        model.addAttribute("projects", projectService.findAll());
+        model.addAttribute("employees", userService.findEmployees());
+        model.addAttribute("tasks", taskService.findAll());
+
 
         return "task/create";
 
