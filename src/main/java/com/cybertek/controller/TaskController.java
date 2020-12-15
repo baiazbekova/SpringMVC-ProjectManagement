e4a4e639552b505ae0917d995e216bbbc3c72d31
abaiaz487 <baiazbekova@gmail.com>
2020-12-15 14:59:36 -0500

    project

--- src/main/java/com/cybertek/controller/TaskController.java
+++ src/main/java/com/cybertek/controller/TaskController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -36,6 +37,15 @@ public class TaskController {
 
 
         return "task/create";
+
+    }
+
+    @PostMapping("/create")
+    public String insertTask(Model model, TaskDTO task){
+
+        taskService.save(task);
+
+        return "redirect:/task/create";
 
     }
 }
